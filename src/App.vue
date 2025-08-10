32dee953440808d39fdddbd268e23efd38675b27
Arturo <arturcas2@hotmail.com>
2025-08-10 15:56:47 -0600

    new component patient

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
 import { ref, reactive } from 'vue';
 import Form from './components/Form.vue';
 import Header from './components/Header.vue';
+import Patient from './components/Patient.vue';
+
 
 const pacients = ref([]);
 
 const paciente = reactive({
-    nombre: '',
-    correo: '',
-    fecha: '',
-    sintomas: ''
+  nombre: '',
+  correo: '',
+  fecha: '',
+  sintomas: ''
 });
 
 const savePacient = () => {
-  pacients.value.push(paciente);
+  pacients.value.push({...paciente});
+  paciente.nombre = '';
 };
 </script>
 
@@ -22,13 +25,18 @@ const savePacient = () => {
     <Header />
 
     <div class="mt-12 md:flex">
-      <Form
-      v-model:nombre="paciente.nombre" v-model:correo="paciente.correo" v-model:fecha="paciente.fecha" v-model:sintomas="paciente.sintomas" 
-      @save-pacient="savePacient"/>
+      <Form v-model:nombre="paciente.nombre" v-model:correo="paciente.correo" v-model:fecha="paciente.fecha"
+        v-model:sintomas="paciente.sintomas" @save-pacient="savePacient" />
 
-      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+      <div class="md:w-1/2 md:h-screen overflow-y-auto scrollbar">
         <h3 class="font-black text-3xl text-center">Administra tus Pacientes</h3>
-        <div v-if="pacients.length > 0"></div>
+        <div v-if="pacients.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de
+            <span class="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          <Patient v-for="(patient, i) in pacients" :key="i" :patient="paciente" />
+        </div>
         <p v-else class="mt-10 text-2xl text-center">No Hay Pacientes</p>
       </div>
     </div>
@@ -36,4 +44,11 @@ const savePacient = () => {
 </template>
 
 <style scoped>
+.scrollbar::-webkit-scrollbar {
+  display: none; /* Oculta la scrollbar en navegadores basados en WebKit (Chrome, Safari) */
+}
+.scrollbar {
+  -ms-overflow-style: none; /* Oculta la scrollbar en Internet Explorer y Edge */
+  scrollbar-width: none; /* Oculta la scrollbar en Firefox */
+}
 </style>
